7c46e6c1386009714e244cc3fc721a3c1259d6db
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-16 20:46:46 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,13 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM cats;"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(tardies) from cats;"
+  "SELECT COUNT(tardies) from students;"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) WHERE grade = 9 from cats;"
+  "SELECT AVG(gpa) WHERE grade = 9 from students;"
 end
